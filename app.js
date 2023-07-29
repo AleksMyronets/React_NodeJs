67c95cc4a2826af27830baa13975550add6e3eaf
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-29 11:19:16 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+import express from 'express';
+import logger  from 'morgan';
+import cors from 'cors'
+import contactsRouter from './routes/contacts.js';
+import userRouter from './routes/users.js';
+
+dotenv.config()
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/contacts', contactsRouter)
+
+app.use('/users', userRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+export default app;
