4c6e108bf65bd88f449eecfb9e55e9b4224a4a8a
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-29 12:44:18 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
+import controllers from '../controllers/users-controllers.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/users/signup')
+userRouter.post('/users/signup', controllers.singUp);
 
 userRouter.post('/users/login')
 
