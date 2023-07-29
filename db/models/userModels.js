65628b534f5175bd1b61c40fdf2c37da21f29702
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-29 12:44:12 +0300

    Update userModels.js

--- db/models/userModels.js
+++ db/models/userModels.js
@@ -1,10 +1,18 @@
 import { Schema, model } from "mongoose";
 
+import bcrypt from 'bcrypt';
+
 const userShema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     token: true
-}, {versionKey: false})
+}, { versionKey: false })
+
+userShema.methods.hashPassword = async function (password) {
+    this.password = await bcrypt.hash(password, 10)
+}
 
 const User = model('user', userShema);
+
+export default User;
